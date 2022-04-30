0eec119c59b0ad7abcca366d2babb0bc2be37df2
anuran <anuranroy02@gmail.com>
2022-04-30 22:48:21 +0530

    Updated hosts

--- translator/settings.py
+++ translator/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rr%+e-=9lxp)u=oe-5x6^l9^m*2+vp(b+2l_2-&drltny05fd#"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["translator-django.herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
